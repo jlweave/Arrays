7e0c412eef0b1c90362b6cf5f9a85c208dfc00bb
Lacey Weaver <jlweave89@gmail.com>
2022-08-03 10:04:34 -0600

    Add the methods

--- arrays.rb
+++ arrays.rb
@@ -1,10 +1,32 @@
-#
+# string, names of employees
 name = [ "Sally", "Bob", "Dan" "Stacey"]
-#
+# integers, students grades
 grades_precentages = [90, 99, 100, 85]
-#
+# floats, outside temputature
 tempurature = [85.2, 99.5, 60.4, 45.3]
-#
+# booleans, will always be either true or false
 bools = [TRUE, FALSE, TRUE, TRUE]
 
-#
+# pop
+name.pop
+grades_precentages.pop
+tempurature.pop
+bools.pop
+
+# push
+name.push
+grades_precentages.push
+tempurature.push
+bools.push
+
+# shift
+name.shift
+grades_precentages.shift
+tempurature.shift
+bools.shift
+
+# unshift
+name.unshift
+grades_precentages.unshift
+tempurature.unshift
+bools.unshift
