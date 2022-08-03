390f5e12c0a2aea33b3c3652a15428857ffca97a
Lacey Weaver <jlweave89@gmail.com>
2022-08-03 10:14:19 -0600

    Adding puts

--- arrays.rb
+++ arrays.rb
@@ -9,24 +9,52 @@ bools = [TRUE, FALSE, TRUE, TRUE]
 
 # pop
 name.pop
+puts name
+
 grades_precentages.pop
+puts grades_precentages
+
 tempurature.pop
+puts tempurature
+
 bools.pop
+puts bools
 
 # push
 name.push
+puts name
+
 grades_precentages.push
+puts grades_precentages
+
 tempurature.push
+puts tempurature
+
 bools.push
+puts bools
 
 # shift
 name.shift
+puts name
+
 grades_precentages.shift
+puts grades_precentages
+
 tempurature.shift
+puts tempurature
+
 bools.shift
+puts bools
 
 # unshift
 name.unshift
+puts name
+
 grades_precentages.unshift
+puts grades_precentages
+
 tempurature.unshift
+puts tempurature
+
 bools.unshift
+puts bools
